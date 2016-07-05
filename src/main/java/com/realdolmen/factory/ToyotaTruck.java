35a45bd62563f048b2a3696bb397cacd62530e78
David Matterne <david.matterne@gmail.com>
2016-07-05 15:00:59 +0200

    Changed model with make values and vice versa

--- src/main/java/com/realdolmen/factory/ToyotaTruck.java
+++ src/main/java/com/realdolmen/factory/ToyotaTruck.java
@@ -2,7 +2,7 @@ package com.realdolmen.factory;
 
 class ToyotaTruck extends Truck {
     public ToyotaTruck(int capacity) {
-        super(capacity, "Toyota", "Super Lorry");
+        super(capacity, "Super Lorry", "Toyota");
     }
 }
 
