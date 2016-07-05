508873bcb2aecb0985905cf8a5759a43bc64dd4c
David Matterne <david.matterne@gmail.com>
2016-07-05 13:32:52 +0200

    Added ChocolateBoiler

---
+++ src/main/java/com.realdolmen.singleton2/ChocolateBoiler.java
@@ -0,0 +1,54 @@
+package com.realdolmen.chocolatefactory;
+
+/**
+ * Created by stannisbaratheon on 05/07/16.
+ */
+public class ChocolateBoiler {
+
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler(boolean empty, boolean boiled) {
+        this.empty = empty;
+        this.boiled = boiled;
+    }
+
+
+    public static ChocolateBoiler getInstance() {
+
+        if (uniqueInstance == null) {
+           uniqueInstance = new ChocolateBoiler(true,false);
+        }
+
+        return uniqueInstance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled=false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
